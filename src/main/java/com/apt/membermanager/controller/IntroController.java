2c93cc7e7a2ffa871bac665041f0e4eaedc28b14
USER <USER@DaYoung>
2026-02-13 14:51:38 +0900

    feat : board게시판

--- src/main/java/com/apt/membermanager/controller/IntroController.java
+++ src/main/java/com/apt/membermanager/controller/IntroController.java
@@ -1,5 +1,13 @@
 package com.apt.membermanager.controller;
 
-public class IntroController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+//구현 위해 임의로 만듬 - 다영
+@Controller
+public class IntroController {
+	@GetMapping("/board/free_list") 
+	public String freeList() {
+		return "board/free_list"; // JSP 파일 이름 반환 }
+	}
 }
